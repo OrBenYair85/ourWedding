b9f4fe0648c0038c84d6ec19a50f7f65b6a5372d
Or Ben Yair <orbenyair@Or-Ben-Yair-VMFJKXVVFV.local>
2025-04-17 21:57:17 +0300

    Starting to build the files of the project

---
+++ src/Pages/SignIn.jsx
@@ -0,0 +1,13 @@
+
+
+export function SignIn () {
+    
+
+    return (
+        <>
+            <form>
+                
+            </form>
+        </>
+    )
+}
---
+++ src/RootCmp.jsx
@@ -0,0 +1,11 @@
+
+
+export function RootCmp () {
+
+
+    return (
+        <>
+            <h1>Hello Our Wedding</h1>
+        </>
+    )
+}
